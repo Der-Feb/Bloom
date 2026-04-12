0af7fb62096b5691d468b0d44dbb73b79026009b
Derick-Feb <ericderickbugingo@gmail.com>
2026-04-12 12:23:50 +0200

    fix: resolving the cors filter

--- backend/src/main/java/derfeb/bloom/BloomApplication.java
+++ backend/src/main/java/derfeb/bloom/BloomApplication.java
@@ -2,12 +2,37 @@ package derfeb.bloom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BloomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BloomApplication.class, args);
+        // cors configuration
+        SpringApplication.run(BloomApplication.class, args);
 	}
 
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:2038", "http://localhost:4200"));
+
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
 }
