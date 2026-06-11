a8988039bb3f4b060d7dc0349f377751b5ba39a2
Der-Feb <bugingoderfeb@gmail.com>
2026-06-11 09:17:38 +0200

    feat: update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { UIComponent } from './app/ui/ui.component';
+import { AppComponent } from './app/app.component';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-bootstrapApplication(UIComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
