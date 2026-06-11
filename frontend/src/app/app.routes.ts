a22f30081f40b8be788a330e3ea83bc31722b88f
Der-Feb <bugingoderfeb@gmail.com>
2026-06-11 09:17:09 +0200

    feat: update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { UIComponent } from './ui/ui.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { LoginComponent } from './auth/login.component';
+import { SignupComponent } from './auth/signup.component';
+import { authGuard, managerGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { 
+    path: 'employees', 
+    component: UIComponent, 
+    canActivate: [authGuard, managerGuard] 
+  },
+  { 
+    path: 'tasks', 
+    component: TasksComponent, 
+    canActivate: [authGuard] 
+  },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '**', redirectTo: '/tasks' }
+];
